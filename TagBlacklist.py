102a6d78a82db7bf951372a7467eb39a0ee4bedf
bigretromike <bigretromike@monogatari.pl>
2017-02-10 12:34:21 +0100

    tagblacklist export;

---
+++ TagBlacklist.py
@@ -0,0 +1,129 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+version = 1  # increase with each push/edit
+
+# special thanks to da3dsoul
+
+tagBlacklistAniDBHelpers = [  # AniDB tags that don't help with anything
+    "body and host",
+    "breasts",
+    "broadcast cropped to 4-3",
+    "cast missing",
+    "cast",
+    "content indicators",
+    "delayed 16-9 broadcast",
+    "description missing",
+    "development hell",  # :( God Eater
+    "dialogue driven",  # anidb and their british spellings
+    "dynamic",
+    "earth",
+    "elements",
+    "ending",
+    "ensemble cast",
+    "family life",
+    "fast-paced",
+    "fictional world",
+    "full hd version available",
+    "jdrama adaptation",
+    "meta tags",
+    "motifs",
+    "multi-anime projects",
+    "noitamina",
+    "origin",
+    "past",
+    "place",
+    "present",
+    "season",
+    "sentai",
+    "setting",
+    "some weird shit goin` on",  # these are some grave accents in use...
+    "storytelling",
+    "tales",
+    "target audience",
+    "technical aspects",
+    "television programme",
+    "themes",
+    "time",
+    "translation convention",
+    "tropes",
+    "ungrouped",
+    "unsorted"
+]
+
+tagBlackListSource = [  # tags containing source of serie
+    "4-koma",
+    "action game",
+    "erotic game",
+    "fan-made",
+    "game",
+    "korean drama",
+    "manga",
+    "manhua",
+    "manhwa",
+    "movie",
+    "new",
+    "novel",
+    "radio programme",
+    "remake",
+    "rpg",
+    "television programme",
+    "ultra jump",
+    "visual novel"
+]
+
+tagBlackListArtStyle = [  # tags that focus on art style
+    "3d cg animation",
+    "3d cg closing",
+    "cel-shaded animation",
+    "cgi",
+    "chibi ed",
+    "experimental animation",
+    "flash animation",
+    "live-action closing",
+    "live-action imagery",
+    "off-model animation",
+    "photographic backgrounds",
+    "product placement",
+    "recycled animation",
+    "slide show animation",
+    "thick line animation",
+    "vignette scenes",
+    "watercolour style",
+    "widescreen transition"
+]
+
+tagBlackListUsefulHelpers = [  # tags that focus on episode attributes
+    "ed variety",
+    "half-length episodes",
+    "long episodes",
+    "multi-segment episodes",
+    "op and ed sung by characters",
+    "op variety",
+    "post-credits scene",
+    "recap in opening",
+    "short episodes",
+    "short movie",
+    "stand-alone movie",
+    "subtle op ed sequence change"
+]
+
+tagBlackListPlotSpoilers = [  # tags that could contain story-line spoilers
+    "branching story",
+    "cliffhangers",
+    "colour coded",
+    "complex storyline",
+    "drastic change in sequel",
+    "fillers",
+    "first girl wins",  # seriously a spoiler
+    "incomplete story",
+    "inconclusive",
+    "inconclusive romantic plot",
+    "non-linear",
+    "open-ended",
+    "room for sequel",
+    "sudden change of pace",
+    "tone changes",
+    "unresolved",
+    "unresolved romance"
+]
